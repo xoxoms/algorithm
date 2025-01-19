cf160d4401e761b8f2072508d6030e8d3bf378ee
JinMyungSung <sdtfqa123@gmail.com>
2025-01-19 22:39:01 +0900

    103. Binary Tree Zigzag Level Order Traversal

--- src/top150/BinaryTreeZigZagLevelOrderTraversal.kt
+++ src/top150/BinaryTreeZigZagLevelOrderTraversal.kt
@@ -7,7 +7,27 @@ import java.util.*
  * 103. Binary Tree Zigzag Level Order Traversal
  */
 class BinaryTreeZigZagLevelOrderTraversal {
+
     fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
+        val result = mutableListOf<MutableList<Int>>()
+        recursive(root ?: return emptyList(), 0, 1, result)
+        return result
+    }
+
+    private fun recursive(treeNode: TreeNode, levelIndex: Int, direction: Int, result: MutableList<MutableList<Int>>) {
+        if (result.size - 1 < levelIndex) { result.add(mutableListOf()) }
+
+        if (direction == 1) {
+            result[levelIndex].add(treeNode.`val`)
+        } else {
+            result[levelIndex].add(0, treeNode.`val`)
+        }
+
+        treeNode.left?.let { recursive(it, levelIndex + 1, direction * -1, result) }
+        treeNode.right?.let { recursive(it, levelIndex + 1, direction * -1, result) }
+    }
+
+    fun zigzagLevelOrderBFS(root: TreeNode?): List<List<Int>> {
         val result = mutableListOf<List<Int>>()
         val queue = LinkedList<TreeNode>()
         queue.add(root ?: return result)
